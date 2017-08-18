ad435c81495ee88a33b2b3f92b1cc9b5c2d01183
ian <macfarlane@temple.edu>
2017-08-18 09:30:12 -0400

    Initial Commit

---
+++ stripe-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stripe-client
+//
+//  Created by Ian MacFarlane on 8/18/17.
+//  Copyright © 2017 Ian MacFarlane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
