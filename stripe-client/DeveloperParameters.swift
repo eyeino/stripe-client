61fce1d316cffd13f93273b92b12963d9312a583
ian <macfarlane@temple.edu>
2017-08-19 17:26:01 -0400

    feat: show response code in UIAlertController

--- stripe-client/DeveloperParameters.swift
+++ stripe-client/DeveloperParameters.swift
@@ -10,9 +10,9 @@ import Foundation
 
 struct DeveloperParameters {
     struct StripeParameters {
-        static let APIKey = "pk_test_86ybhlRNQgVLyNlBjTp33Rqp"
-        //static let requestString = "https://fierce-headland-91323.herokuapp.com/create_charge"
+        static let APIKey = "pk_test_n8om9CiAtCg5c81gDocbnLrV"
+        static let requestString = "https://fierce-headland-91323.herokuapp.com/create_charge"
         
-        static let requestString = "https://vast-retreat-67066.herokuapp.com/charge.php"
+        //static let requestString = "https://vast-retreat-67066.herokuapp.com/charge.php"
     }
 }
--- stripe-client/ViewController.swift
+++ stripe-client/ViewController.swift
@@ -49,25 +49,38 @@ class ViewController: UIViewController, STPPaymentCardTextFieldDelegate {
     
     func chargeUsingToken(token: STPToken) {
         let requestString = DeveloperParameters.StripeParameters.requestString
-        let parameters = ["stripeToken": token.tokenId, "amount": "200", "currency": "usd", "description": "testRun"]
+        let parameters = ["token": token.tokenId, "amount": "200", "currency": "usd", "description": "testRun"]
         
-        Alamofire.request(requestString, method: .post, parameters: parameters).responseJSON { response in
+        Alamofire.request(requestString, method: .post, parameters: parameters, encoding: URLEncoding.default).responseJSON { response in
             if let request = response.request {
+                print("----REQUEST----")
                 print(request)
             }
             
             if let response = response.response {
+                print("----RESPONSE----")
                 print(response)
+                print(parameters)
             }
             
             if let data = response.data {
-                print(data)
+                print("----DATA----")
+                print(String(data: data, encoding: String.Encoding.utf8)!)
             }
             
+            print("----RESPONSE.RESULT----")
             print(response.result)
             
             if let JSON = response.result.value {
                 print("JSON: \(JSON)")
+            }
+            
+            if let code = response.response?.statusCode {
+                DispatchQueue.main.async {
+                    let alert = UIAlertController(title: "Response received!", message: "The server returned status code \(code).", preferredStyle: UIAlertControllerStyle.actionSheet)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
             }
         }
     }
